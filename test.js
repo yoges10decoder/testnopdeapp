d1d3b69c405415b97264287007c23298e0973a95
yoges10decoder <yogeskumar@10decoders.in>
2022-05-13 07:05:46 +0000

    initial commit

---
+++ test.js
@@ -0,0 +1,10 @@
+# create a file named test.js with the following contents
+const assert = require('assert');
+describe('Simple Math Test', () => {
+ it('should return 2', () => {
+        assert.equal(1 + 1, 2);
+    });
+ it('should return 9', () => {
+        assert.equal(3 * 3, 9);
+    });
+});
